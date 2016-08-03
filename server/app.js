f092e736eb04c16daa5da5ac367361539b73cad8
dskrenta <dskrenta@gmail.com>
2016-08-03 05:29:40 -0700

    added socket observable api

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 import Koa from 'koa';
 import fs from 'fs';
 import serveStatic from 'koa-serve-static';
+import Socket from './transport';
 import router from './routes/demo';
 
 const app = new Koa();
@@ -11,6 +12,7 @@ app
   .use(router.routes())
   .use(serveStatic(`${__dirname}/public`));
 
+const socket = Socket(app);
 app.listen(PORT, () => console.log(`Server started on *:${PORT}`));
 
 export default app;
