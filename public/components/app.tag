3b0673c1fbe887cb0f03b6f45885eb1cff2eebbd
dskrenta <dskrenta@gmail.com>
2016-08-07 04:13:21 -0700

    added demo routing example

---
+++ public/components/app.tag
@@ -0,0 +1,7 @@
+<app>
+  <h1>Base app tag</h1>
+  <p>Add links to riot routes</p>
+
+  <script>
+  </script>
+</app>
--- public/main.js
+++ public/main.js
@@ -1,30 +1,28 @@
 import riot from 'riot';
 
-import sayHello from './lib/sayHello';
-import Person from './lib/person';
-import Coord from './lib/coord';
-import demo from './lib/demo';
-
 import './components/sample.tag';
+import './components/app.tag';
 
-riot.mount('sample');
+riot.route.stop();
+riot.route.start();
 
-sayHello('David');
+var currentPage = null;
 
-console.log('Some more stuff');
+const routes = {
+  home: (action, id) => {
+    var currentPage = riot.mount('#app', 'app');
+  },
+  sample: (action, id) => {
+    var currentPage = riot.mount('#sample', 'sample');
+  }
+};
 
-const david = new Person('David', 17, 200, 165);
-const point = new Coord(100, 100, 'pizza');
+function handler (collection, id, action) {
+  if (currentPage) currentPage.unmount(true);
 
-let stuff = david.toString();
-console.log(stuff);
-point.toString();
+  const fn = routes[collection || 'home'];
+  return fn ? fn(id, action) : console.error('no route found: ', collection, id, action);
+}
 
-;(() => {
-  setTimeout(() => {
-    sweetAlert("Oops...", "Something went wrong!", "error");
-  }, 2000);
-})();
-
-demo()
-  .then((data) => console.log(data));
+riot.route(handler);
+riot.route.exec(handler);
