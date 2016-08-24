bdb646a5f405593439394bd367a9b52f93d82a16
BDesBrisay <bryce@desbrisay.com>
2016-08-23 23:23:48 -0700

    created sample table for stock articles adn linked spectre css

--- public/components/app.tag
+++ public/components/app.tag
@@ -1,6 +1,7 @@
 <app>
   <a href="#/login">login</a>
   <a href="#/mapview">mapview</a>
+  <a href="#/article-table">article</a>
 
   <div id="view"></div>
 
--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ import riot from 'riot';
 import './components/app.tag';
 import './components/login.tag';
 import './components/mapview.tag';
+import './components/article-table.tag';
 
 riot.mount('app');
 riot.route.start(true);
