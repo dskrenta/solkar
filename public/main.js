1ce39aef252723410d05f130a454823ba44a0b78
BDesBrisay <bryce@desbrisay.com>
2016-08-28 20:36:43 -0700

    changed from spectre to bootstrap

--- public/main.js
+++ public/main.js
@@ -3,7 +3,6 @@ import './components/app.tag';
 import './components/login.tag';
 import './components/mapview.tag';
 import './components/article-table.tag';
-import './components/dash-header.tag';
 
 riot.mount('app');
 riot.route.start(true);
