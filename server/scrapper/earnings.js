b9684022f38e718a522477cb0237faac9b147860
dskrenta <dskrenta@gmail.com>
2016-09-01 13:15:28 -0700

    add entire quoteData object in earnings object

--- server/scrapper/earnings.js
+++ server/scrapper/earnings.js
@@ -2,15 +2,16 @@
 import request from 'request';
 import cheerio from 'cheerio';
 import yahooFinance from 'yahoo-finance';
+import loadDOM from './scrape';
 
 export default async function earnings (url) {
   try {
     const $ = await loadDOM('https://biz.yahoo.com/research/earncal/today.html');
     const data = await parseEarnings($);
-    const modData = data.map(elem => getAverageVolume(elem.symbol));
-    const averageDailyVolumes = await Promise.all(modData);
+    const modData = data.map(elem => getQuoteInfo(elem.symbol));
+    const quoteData = await Promise.all(modData);
     for (let i = 0; i < data.length; i++) {
-      data[i]['averageDailyVolume'] = averageDailyVolumes[i];
+      data[i]['quoteData'] = quoteData[i];
     }
     const volumeSortedData = volumeSort(data);
     return volumeSortedData.reverse();
@@ -19,39 +20,11 @@ export default async function earnings (url) {
   }
 }
 
-function loadDOM (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(cheerio.load(body));
-      } else {
-        reject(error);
-      }
-    })
-  });
-}
-
-function volumeSort (arr) {
-  return arr.sort((a, b) => {
-    let keyA = a.averageDailyVolume;
-    let keyB = b.averageDailyVolume;
-    if (keyA < keyB) return -1;
-    if (keyA > keyB) return 1;
-    return 0;
-  });
-}
-
 function getQuoteInfo (symbol, fields) {
   return yahooFinance.snapshot({
     symbol: symbol,
     fields: fields
   })
-}
-
-function getAverageVolume (symbol) {
-  return new Promise((resolve, reject) => {
-    getQuoteInfo(symbol, ['a2']).then(result => resolve(result.averageDailyVolume));
-  });
 }
 
 function parseEarnings ($) {
