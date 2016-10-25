65034079e7ab19286f052747c2e8f9a05afce3ae
dskrenta <dskrenta@gmail.com>
2016-10-24 21:45:57 -0700

    fix merge conflicts

--- server/scrapper/test.js
+++ server/scrapper/test.js
@@ -1,6 +1,6 @@
 import { earnings, getEarningsHistory, getEarningsResearch } from './earnings';
 
-earnings('20161014')
+earnings('20161110')
   .then((result) => {
     console.log(JSON.stringify(result, null, '\t'));
   })
