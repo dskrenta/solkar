9681ac1d30319df92880e2a3b1a09a43fab2b9f1
dskrenta <dskrenta@gmail.com>
2016-12-22 18:35:56 -0800

    annualized risk free interest rate api function

--- server/scrapper/risk-free-interest.js
+++ server/scrapper/risk-free-interest.js
@@ -14,6 +14,11 @@ function parseRate ($) {
   return new Promise((resolve, reject) => {
     let rate = $('.listSumry').children().first().text();
     rate = rate.match(/\d+.\d+/)[0];
+    rate /= 100;
+    rate += 1;
+    rate = Math.pow(rate, 4);
+    rate -= 1;
+    rate *=  100;
     resolve(rate);
   });
 }
