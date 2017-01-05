b3255d9d441f17ec707c8f90a9c6cd0bf70b4e38
dskrenta <dskrenta@gmail.com>
2017-01-04 17:42:04 -0800

    basic iron condor generator

---
+++ server/api/iron-condor.js
@@ -0,0 +1,46 @@
+'use strict';
+import options from './options-chain';
+
+export default async function ironCondor (symbol) {
+  try {
+    let optionChain = await options(symbol);
+    const shortCalls = optionChain.optionChain.result[0].options[0].calls.filter(call => !call.inTheMoney);
+    const shortPuts = optionChain.optionChain.result[0].options[0].puts.filter(put => !put.inTheMoney).reverse();
+    const ironCondors = [];
+
+    for (let i = 0; i < shortCalls.length; i++) {
+      const longPuts = shortPuts.slice(i + 1, shortPuts.length);
+      const longCalls = shortCalls.slice(i + 1, shortCalls.length);
+
+      for (let j = 0; j < longCalls.length; j++) {
+        const maxProfit = shortCalls[i].lastPrice + shortPuts[i].lastPrice;
+        const maxLoss = longCalls[j].strike - shortCalls[i].strike - maxProfit;
+        const upperBreakeven = shortCalls[i].strike + maxProfit;
+        const lowerBreakeven = shortPuts[i].strike - maxProfit;
+        const probabilityOfProfit = maxLoss / (maxProfit + maxLoss);
+        const riskRewardRatio = maxProfit / maxLoss;
+
+        const ironCondor = {
+          contracts: {
+            shortCall: shortCalls[i],
+            shortPut: shortPuts[i],
+            longCall: longCalls[j],
+            longPut: longPuts[j]
+          },
+          maxProfit: maxProfit,
+          maxLoss: maxLoss,
+          upperBreakeven: upperBreakeven,
+          lowerBreakeven: lowerBreakeven,
+          probabilityOfProfit: probabilityOfProfit,
+          riskRewardRatio: riskRewardRatio
+        };
+
+        ironCondors.push(ironCondor);
+      }
+    }
+
+    return ironCondors;
+  } catch (err) {
+    console.log(err);
+  }
+}
