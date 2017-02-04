2a4311c3c18f5bd9bce43b2c0f4f6029f6646860
dskrenta <dskrenta@gmail.com>
2017-02-04 06:23:42 -0800

    historical data json downloader, ml dir, aapl historical data from 2000 to 2014

---
+++ server/scraper/historical-data.js
@@ -0,0 +1,19 @@
+'use strict';
+import yahooFinance from 'yahoo-finance';
+
+function getHistoricalData (symbol, startDate, endDate) {
+  return yahooFinance.historical({
+    symbol: symbol,
+    from: startDate,
+    to: endDate
+  });
+}
+
+getHistoricalData('aapl', '2000-1-1', '2015-1-1')
+  .then((result) => {
+    // console.log(JSON.stringify(result, null, '\t'));
+    console.log(JSON.stringify(result));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
