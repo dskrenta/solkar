3bce2c37079ce4449cc43eb9dea38195485b54ef
dskrenta <dskrenta@gmail.com>
2017-02-07 00:00:51 -0800

    abstract api demo

--- ml/test.py
+++ ml/test.py
@@ -1,14 +1,18 @@
 import numpy as np
-import talib
+from talib.abstract import *
 
-close = np.random.random(100)
-output = talib.SMA(close)
-mom = talib.MOM(close, timeperiod=5)
+inputs = {
+    'open': np.random.random(100),
+    'high': np.random.random(100),
+    'low': np.random.random(100),
+    'close': np.random.random(100),
+    'volume': np.random.random(100)
+}
 
-print(output)
-print(mom)
+output = SMA(inputs, timeperiod=25) # calculate on close prices by default
+output = SMA(inputs, timeperiod=25, price='open') # calculate on opens
+upper, middle, lower = BBANDS(inputs, 20, 2, 2)
+slowk, slowd = STOCH(inputs, 5, 3, 0, 3, 0) # uses high, low, close by default
+slowk, slowd = STOCH(inputs, 5, 3, 0, 3, 0, prices=['high', 'low', 'open'])
 
-upper, middle, lower = talib.BBANDS(close, matype=talib.MA_Type.T3)
-print(upper, middle, lower)
-
-print(talib.get_functions())
+print(output, upper, middle, lower, slowk, slowd)
