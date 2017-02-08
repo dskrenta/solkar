5a7a8aa7a2bdde28178916def6cac9951a6da9ff
dskrenta <dskrenta@gmail.com>
2017-02-07 17:57:13 -0800

    demo data manipulation for sklearn

---
+++ ml/data.py
@@ -0,0 +1,57 @@
+import json
+import numpy as np
+from talib.abstract import *
+# import matplotlib.pyplot as plt
+
+def get_data(file_name):
+    data_file = open(file_name, encoding='utf-8')
+    data = json.loads(data_file.read())
+    return data
+
+def format_data(data):
+    return {
+        'open': np.array(list(map(lambda d: d['open'], data)), dtype='f8'),
+        'high': np.array(list(map(lambda d: d['high'], data)), dtype='f8'),
+        'low': np.array(list(map(lambda d: d['low'], data)), dtype='f8'),
+        'close': np.array(list(map(lambda d: d['close'], data)), dtype='f8'),
+        'volume': np.array(list(map(lambda d: d['volume'], data)), dtype='f8')
+    }
+
+def add_indicators(inputs):
+    # Overlap Studies
+    sma5 = SMA(inputs, timeperiod=5)
+    sma10 = SMA(inputs, timeperiod=10)
+    ema5 = EMA(inputs, timeperiod=5)
+    ema10 = EMA(inputs, timeperiod=10)
+
+    # Momentum Indicators
+    rsi = RSI(inputs, timeperiod=14)
+    mfi = MFI(inputs, timeperiod=14)
+    adx = ADX(inputs, timeperiod=14)
+    willr = WILLR(inputs, timeperiod=14)
+    ultosc = ULTOSC(inputs, timeperiod1=7, timeperiod2=14, timeperiod3=28)
+    aroondown, aroonup = AROON(inputs, timeperiod=14)
+    aroonosc = AROONOSC(inputs, timeperiod=14)
+    cmo = CMO(inputs, timeperiod=14)
+    macd, macdsignal, macdhist = MACD(inputs, fastperiod=12, slowperiod=26, signalperiod=9)
+    slowk, slowd = STOCH(inputs, fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+
+    # Volume Indicators
+    obv = OBV(inputs)
+
+    # Volatility Indicators
+    atr = ATR(inputs, timeperiod=14)
+    natr = NATR(inputs, timeperiod=14)
+    trange = TRANGE(inputs)
+
+    return np.column_stack([sma5, sma10, ema5, ema10, rsi, mfi, adx, willr, ultosc, aroondown,
+    aroonup, aroonosc, cmo, macd, macdsignal, macdhist, slowk, slowd, obv, atr, natr, trange])
+
+data = get_data('aapl-historical-2000-2014.json')
+inputs = format_data(data)
+
+X = add_indicators(inputs)
+Y = inputs['close']
+
+print(X)
+print(Y)
--- ml/numpy-test.py
+++ ml/numpy-test.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-a = np.array = ([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+a = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
 
-print(a[0])
-print(a[0][1])
+# print(a[0])
+# print(a[0][0])
+
+b = np.array([1, 2, 3])
+c = np.array([4, 5, 6])
+d = np.array([7, 8, 9])
+
+e = np.column_stack(([b, c, d]))
+print(e)
