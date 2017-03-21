3d001e23b6b910e8f3bfb4fc087e3ea3345dd662
dskrenta <dskrenta@gmail.com>
2017-03-21 10:19:38 -0700

    realtime data

--- app/app.js
+++ app/app.js
@@ -9,11 +9,19 @@ import yahooFinance from 'yahoo-finance';
 import * as lib from './lib/lib.js';
 import './components/components.js';
 
-const socket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/last');
+const socket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/tops');
 const observe = riot.observable();
+let currentSymbol;
+
+socket.on('message', message => observe.trigger('quote-update:realtime', message));
+
+socket.on('disconnect', () => console.log('Disconnected.'))
 
 observe.on('quote-select', symbol => {
+  if (currentSymbol) socket.emit('unsubscribe', currentSymbol);
+  currentSymbol = symbol;
   lib.getData(symbol);
+  socket.emit('subscribe', symbol);
 });
 
 riot.mount('main');
